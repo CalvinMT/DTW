63f9eea966e55c9e9ba29647d90c628866f3ccf3
CalvinMT <calvin.massonnet@gmail.com>
2021-06-21 13:54:51 +0200

    Feature confusion matrix from sequences

--- stats.py
+++ stats.py
@@ -93,6 +93,54 @@ def getConfusionMatrix(results, expectations):
             r += 1
     return TP, TN, FP, FN
 
+def getConfusionMatrixWithSequences(results, expectations):
+    """
+    Based on DyLNet transcription structure.
+
+    TODO
+    Compare results with expectations and return the number of true positive (TP),
+    true negative (TN), false positive (FP), false negative (FN) respectively.
+    The results array and the expectations array don't have to be of the same length,
+    though the results array's length have to be equal or higher than the expectations
+    array's length.
+
+    :param results: array of obtained results
+    :param expectations: array of expected results
+    :return: tuple of four floats
+    """
+    assert len(results) >= len(expectations)
+    TP = 0.0
+    TN = 0.0
+    FP = 0.0
+    FN = 0.0
+    for i in range(len(results)):
+        r = 0
+        e = 0
+        while r < len(results[i]) or (i < len(expectations) and e < len(expectations[i])):
+            if i >= len(expectations) or e >= len(expectations[i]) or (r < len(results[i]) and results[i][r][0] < expectations[i][e][0][0]):
+                if results[i][r][1]:
+                    FP += 1.0
+                else:
+                    TN += 1.0
+            elif r >= len(results[i]) or (e < len(expectations[i]) and results[i][r][0] > expectations[i][e][0][1]):
+                FN += 1.0
+                r -= 1
+                e += 1
+            else:
+                if results[i][r][1] == expectations[i][e][1]:
+                    if results[i][r][1]:
+                        TP += 1.0
+                    else:
+                        TN += 1.0
+                else:
+                    if results[i][r][1]:
+                        FP += 1.0
+                    else:
+                        FN += 1.0
+                e += 1
+            r += 1
+    return TP, TN, FP, FN
+
 def getTPRAndFPR(TP, TN, FP, FN):
     """
     Compute and return the true positive rate (TPR) and the false positive rate (FPR)
@@ -113,7 +161,7 @@ def getTPRAndFPR(TP, TN, FP, FN):
         FPR = TN / (TN + FP)
     return TPR, FPR
 
-def computeROCCurve(sweepList, expectations, nbThresholds=100, positiveOnly=False, oneWord=False):
+def computeROCCurve(sweepList, expectations, nbThresholds=100, positiveOnly=False, oneWord=False, inSequence=False):
     """
     TODO
 
@@ -124,7 +172,10 @@ def computeROCCurve(sweepList, expectations, nbThresholds=100, positiveOnly=Fals
 
     for threshold in thresholdList:
         results = computeResults(sweepList, threshold, positiveOnly, oneWord)
-        TP, TN, FP, FN = getConfusionMatrix(results, expectations)
+        if inSequence:
+            TP, TN, FP, FN = getConfusionMatrixWithSequences(results, expectations)
+        else:
+            TP, TN, FP, FN = getConfusionMatrix(results, expectations)
         TPR, FPR = getTPRAndFPR(TP, TN, FP, FN)
         rocPoints.append([TPR, FPR])
     
@@ -146,7 +197,7 @@ def showROCCurve(AUC, pivot, figure=None, plotpos=[1, 1, 1], title="Receiver Ope
     ax = figure.add_subplot(plotpos[0], plotpos[1], plotpos[2])
     if title:
         ax.set_title(title + "\n(AUC=" + str(round(AUC, 4)) + ")")
-    ax.plot(np.array(1-pivot.x), np.array(pivot.y))
+    ax.plot(np.array(1-pivot.y), np.array(pivot.x))
     ax.plot([0, 1],'r--')
     ax.set_xlabel("False Positive Rate")
     ax.set_ylabel("True Positive Rate")
